b9328319ce3e8800e2e867103d60d6b834acf801
Nishant Niranjan <awsUserNishant.gmail.com>
2020-12-30 02:05:23 +0530

    file adding and merged

--- GitBasicProj/src/main/java/org/example/App.java
+++ GitBasicProj/src/main/java/org/example/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! f1 master updated" );
+        System.out.println("This is f4 updation");
     }
 }
